dc07293316e27834f5064c05a2494a19a8b1231a
Daniel Munoz <daniel.munozdev@gmail.com>
2018-12-02 15:49:49 -0800

    add solution

--- main.js
+++ main.js
@@ -11,3 +11,29 @@
 // print the ratio of positive numbers rounded to the 6th decimal place
 // print the ratio of negative numbers rounded to the 6th decimal place
 // print the ratio of zeros rounded to the 6th decimal place
+
+
+function returnPlusMinusRatios(numbers) {
+  // counter variables for positive, negative and zero numbers
+  let positiveNumberCounter = 0;
+  let negativeNumberCounter = 0;
+  let zeroNumberCounter = 0;
+
+  // check if each item of the array is positive, negative, or zero
+  // add a counter to the corresponding variable
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+      positiveNumberCounter++;
+    } else if (numbers[i] === 0) {
+      zeroNumberCounter++;
+    } else {
+      negativeNumberCounter++;
+    }
+  }
+  // log the ratio of positive, negative and zero numbers
+  console.log((positiveNumberCounter / numbers.length).toPrecision(6));
+  console.log((negativeNumberCounter / numbers.length).toPrecision(6));
+  console.log((zeroNumberCounter / numbers.length).toPrecision(6));
+}
+
+returnPlusMinusRatios([1, 2, 4, -1, -2, 0]);
